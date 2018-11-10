bc61ea01585c8758a00719cf47f93bd872810ea9
Pranay Neelagiri <pneeldam@gmail.com>
2018-11-10 13:48:48 -0500

    Add kwd fetching

--- server/app/views.py
+++ server/app/views.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from app.service import get_keywords_from_url
+import json
 
 mod = Blueprint('service', __name__, url_prefix='/service')
 
 @mod.route('/relevant-articles', methods=['GET'])
 def find_relevant_articles():
-    return None
+    url = request.args.get('url')
+    if url is not None:
+        return jsonify(
+            {
+                "articles": [],
+                "keywords": get_keywords_from_url(url)
+            }
+        )
+    return jsonify(
+        {
+            "articles": [],
+            "keywords": []
+        }
+    )
