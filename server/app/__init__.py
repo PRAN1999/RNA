3e7f6bfcd8bf0cf40a20bd8787d75e5fafe35710
Pranay Neelagiri <pneeldam@gmail.com>
2018-11-10 11:28:09 -0500

    API configuration for watson and news

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -6,3 +6,13 @@ app.secret_key = 'rna_secret'
 from app import views
 
 app.register_blueprint(views.mod)
+
+from configparser import ConfigParser
+from app.constants import *
+
+parser = ConfigParser()
+parser.read('../config.ini')
+
+watson_url = parser[WATSON_SECTION][WATSON_URL]
+watson_api_key = parser[WATSON_SECTION][WATSON_API_KEY]
+news_api_key = parser[NEWS_API_SECTION][NEWS_API_KEY]
---
+++ server/app/constants.py
@@ -0,0 +1,6 @@
+WATSON_SECTION = 'Watson Credentials'
+WATSON_URL = 'watson_url'
+WATSON_API_KEY = 'watson_api_key'
+
+NEWS_API_SECTION = 'NewsAPI Credentials'
+NEWS_API_KEY = 'news_api_key'
