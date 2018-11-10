ae422564f11bec304d7868138ed54f93386d724c
Shoumyo <shoumyoarchit@gmail.com>
2018-11-10 09:48:39 -0500

    Updated React App to be able to create separate popup window, created page source scraper

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './rna.png';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">RNA</h1>
         </header>
+        <br />
+        <p className="App-intro">
+          <small>
+            To get started, just open up any <code>Reddit</code> text post, and we'll find any links we find for you.
+          </small>
+        </p>
+        <div className="App-body">
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,26 @@
+/*global chrome*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if(!chrome.storage) {
+    let items = {
+        reddit_data: {
+            
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    ReactDOM.render(<App />, 
+        document.getElementById('root'));
+    registerServiceWorker();
+} else {
+    chrome.storage.sync.get(['reddit_data'], function(items) {
+        console.log(items)
+        ReactDOM.render(<App reddit-data={items['reddit_data']}/>, 
+            document.getElementById('root'));
+        registerServiceWorker();
+    });
+}
