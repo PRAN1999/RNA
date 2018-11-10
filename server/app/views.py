9989a443048db86993becccc9f1e40cf383805a8
Pranay Neelagiri <pneeldam@gmail.com>
2018-11-10 14:11:33 -0500

    add endpoint logic

--- server/app/views.py
+++ server/app/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.service import get_keywords_from_url
+from app.service import get_keywords_from_url, get_articles_from_keywords
 import json
 
 mod = Blueprint('service', __name__, url_prefix='/service')
@@ -7,16 +7,16 @@ mod = Blueprint('service', __name__, url_prefix='/service')
 @mod.route('/relevant-articles', methods=['GET'])
 def find_relevant_articles():
     url = request.args.get('url')
+    kwd_args = request.args.getlist('kwd')
+    kwds = []
     if url is not None:
-        return jsonify(
-            {
-                "articles": [],
-                "keywords": get_keywords_from_url(url)
-            }
-        )
+        kwds = get_keywords_from_url(url)
+    elif kwd_args.count != 0:
+        kwds = kwd_args
+    articles = get_articles_from_keywords(kwds)
     return jsonify(
         {
-            "articles": [],
-            "keywords": []
+            "articles": articles,
+            "keywords": kwds
         }
     )
