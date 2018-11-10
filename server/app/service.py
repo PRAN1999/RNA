c8dd4e8e011200a566c1045e2f4514a69f95f279
Adithya Prahlad <adithyaprahlad@nat-tvwna-inside-visitornet101-b-7560.princeton.org>
2018-11-10 13:53:22 -0500

    calculate datetime

--- server/app/service.py
+++ server/app/service.py
@@ -1,15 +1,20 @@
 from app.models.article import Article
 from newsapi import NewsApiClient
 from app import news_api_key
+from datetime import datetime, timedelta
+
+
 
 #Init
 newsapi = NewsApiClient(api_key = news_api_key)
-
-#keywords must
 def get_articles_from_keywords(keywords):
+    now = datetime.datetime.now().replace(microsecond=0).isoformat()
+    print((now))
+    then = now - timedelta(days=7)
+    print(then)
+
     all_articles = newsapi.get_everything(
         q=keywords,
-        sources='abc-news,',
         language='en',
         sort_by='relevancy' 
     )
